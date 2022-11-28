ab370cb522582a9ebd279701dbae8b05ee002fd7
kimminsic <alstlr1135@gmail.com>
2022-11-28 23:33:19 +0900

    buyitem entity 수정

--- src/main/java/com/task/taskInfo/dao/BuyitemInfo.java
+++ src/main/java/com/task/taskInfo/dao/BuyitemInfo.java
@@ -1,23 +1,31 @@
 package com.task.taskInfo.dao;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @Entity
+@IdClass(BuyitemInfo.BuyitemInfoKey.class)
 public class BuyitemInfo {
-    @OneToOne
-    @JoinColumn(name="itemName",insertable = false,updatable = false)
-    private ProductInfo ItemInfo;
 
-    @OneToOne
-    @JoinColumn(name="tel",insertable = false,updatable = false)
-    private BuyerInfo buyer;
+    @EqualsAndHashCode
+    @Embeddable
+    static class BuyitemInfoKey implements Serializable {
+        @OneToOne
+        @JoinColumn(name = "itemName", insertable = false, updatable = false)
+        private ProductInfo ItemInfo;
+
+        @OneToOne
+        @JoinColumn(name = "tel", insertable = false, updatable = false)
+        private BuyerInfo buyer;
+    }
 
     private String price;
 
