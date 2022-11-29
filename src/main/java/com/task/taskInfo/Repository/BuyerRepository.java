de04cc7a2355d256e46eb26ab55117f69f09beb2
kimminsic <alstlr1135@gmail.com>
2022-11-29 18:52:00 +0900

    select 문 수정

--- src/main/java/com/task/taskInfo/Repository/BuyerRepository.java
+++ src/main/java/com/task/taskInfo/Repository/BuyerRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
 
 public interface BuyerRepository extends JpaRepository<BuyerInfo,Integer> {
 
-    @Query(value = "select buyer_name,concat(substr(tel,1,10),'**',substr(tel,12,1)) as tel from buyer_info", nativeQuery = true)
+    @Query(value = "select buyer_name,concat(substr(tel,1,10),'**',substr(tel,13,1)) as tel from buyer_info", nativeQuery = true)
     List<BuyerInfo> getList();
 }
